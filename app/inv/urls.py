99dbd7c986cae76f22269c7c6f175d0a48a2c150
alberto <scontrerasa3@miumg.edu.gt>
2023-10-09 11:58:24 -0600

    :egg: Seccion Productos terminado

--- app/inv/urls.py
+++ app/inv/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel, SubCategoriaView, SubCategoriaNew, SubCategoriaEdit, SubCategoriaDel, MarcaView, MarcaNew, MarcaEdit, marca_inactivar, UMView, UMNew, UMEdit, um_inactivar
+from .views import CategoriaView, CategoriaNew, CategoriaEdit, CategoriaDel, SubCategoriaView, SubCategoriaNew, SubCategoriaEdit, SubCategoriaDel, MarcaView, MarcaNew, MarcaEdit, marca_inactivar, UMView, UMNew, UMEdit, um_inactivar, ProductoView, ProductoNew, ProductoEdit, producto_inactivar
 
 urlpatterns = [
     path('categorias/', CategoriaView.as_view(), name='categoria_list'),
@@ -22,6 +22,12 @@ urlpatterns = [
     path('um/new', UMNew.as_view(), name='um_new'),
     path('um/edit/<int:pk>', UMEdit.as_view(), name='um_edit'),
     path('um/inactivar/<int:id>', um_inactivar, name='um_inactivar'),
+
+    path('producto/', ProductoView.as_view(), name='producto_list'),
+    path('producto/new', ProductoNew.as_view(), name='producto_new'),
+    path('producto/edit/<int:pk>', ProductoEdit.as_view(), name='producto_edit'),
+    path('producto/inactivar/<int:id>', producto_inactivar, name='producto_inactivar'),
+
 
 
 
